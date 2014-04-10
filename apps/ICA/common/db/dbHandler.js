a186e2cac10d8c67012d2c825bf9373f24f32591
Robin Chowdhury <robinch88@gmail.com>
2014-04-10 16:05:22 +0200

    Added getters for json objects

--- apps/ICA/common/db/dbHandler.js
+++ apps/ICA/common/db/dbHandler.js
@@ -3,11 +3,55 @@ function dbHandler(){
 	this.users = db.getUsers();
 	this.groceries = db.getGroceries;
 	
-	this.getUserFavorites = function(userId){
+	this.getUserFavoriteObjects = function(userId){
+		var favoriteList = new Array();
+		var userFavorites = this.getUser(userId).userFavorites;		
+				for(var listItem in userFavorites) {
+					favoriteList.add(this.getUser(userFavorites[listItem]));					
+		}
+		return favoriteList;
+	};
+	
+	this.getUserFavoriteGrocery = function(userId){
+		var favoriteList = new Array();
+		var userFavorites = this.getUser(userId).favoriteGroceries;		
+				for(var listItem in userFavorites) {
+					favoriteList.add(this.getGrocery(userFavorites[listItem]));					
+		}
+		return favoriteList;
+	};
+	
+	this.getUserRecentGrocery = function(userId){
+		var recentList = new Array();
+		var userRecents = this.getUser(userId).recentBoughtGroceries;		
+				for(var listItem in userRecents) {
+					recentList.add(this.getGrocery(userRecents[listItem]));					
+		}
+		return recentList;
+	};
+	
+	this.getUserFavoriteObjects = function(userId){
+		var favoriteList = new Array();
+		var userFavorites = this.getUser(userId).userFavorites;		
+				for(var listItem in userFavorites) {
+					favoriteList.add(this.getUser(userFavorites[listItem]));					
+		}
+		return favoriteList;
+	};
+	
+	this.getUser = function(userId) {
 		for(var user in this.users) {
 			if(this.users[user].uid == userId) {
-				return this.users[user].userFavorites;
-			}	
+				 return this.users[user];
+			}
+		}
+	};
+	
+	this.getGrocery = function(grocId) {
+		for(var groc in this.groceries) {
+			if(this.groceries[groc].id == grocId) {
+				 return this.groceries[groc];
+			}
 		}
 	};
 	
@@ -17,5 +61,21 @@ function dbHandler(){
 	
 	this.getAllUsers = function(){
 		return this.users;
+	};
+	
+	this.getFirstNameUser = function(userObject) {
+		return userObject.firstName;
+	};
+	
+	this.getLastNameUser = function(userObject) {
+		return userObject.lastName;
+	};
+	
+	this.getAgeUser = function(userObject) {
+		return userObject.age;
+	};
+	
+	this.getCategoryGroc = function(grocObject) {
+		return grocObject.category;
 	};
 }
