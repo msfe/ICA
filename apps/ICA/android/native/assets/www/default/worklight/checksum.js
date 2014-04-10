8203567f467712b190c7f624074e1454314f589e
Mattias <Mattias.Folke@gmail.com>
2014-04-10 19:11:38 +0200

    using pages, javascript fucked up

--- apps/ICA/android/native/assets/www/default/worklight/checksum.js
+++ apps/ICA/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":1553375134,"date":1397144047002,"machine":"MattiasDator"};
-/* Date: Thu Apr 10 17:34:07 CEST 2014 */
+var WL_CHECKSUM = {"checksum":249590306,"date":1397149790150,"machine":"MattiasDator"};
+/* Date: Thu Apr 10 19:09:50 CEST 2014 */
