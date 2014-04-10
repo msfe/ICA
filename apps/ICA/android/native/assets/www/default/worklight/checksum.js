a3e584da7ddb40b383c467a80bde64971e410578
Mattias <Mattias.Folke@gmail.com>
2014-04-10 15:20:40 +0200

    fixed add page

--- apps/ICA/android/native/assets/www/default/worklight/checksum.js
+++ apps/ICA/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":1899237796,"date":1397134288960,"machine":"MattiasDator"};
-/* Date: Thu Apr 10 14:51:28 CEST 2014 */
+var WL_CHECKSUM = {"checksum":3406516747,"date":1397135945033,"machine":"MattiasDator"};
+/* Date: Thu Apr 10 15:19:05 CEST 2014 */
