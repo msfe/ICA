bb3b3105f402226e3cbee015deee123291066b30
Robin Chowdhury <robinch88@gmail.com>
2014-04-10 15:28:31 +0200

    Added getters in dbHandler

--- apps/ICA/common/db/dbHandler.js
+++ apps/ICA/common/db/dbHandler.js
@@ -1,13 +1,21 @@
 function dbHandler(){
 	this.db = new FakeDb();
-	var users = db.getUsers();
+	this.users = db.getUsers();
 	this.groceries = db.getGroceries;
 	
 	this.getUserFavorites = function(userId){
-		for(var user in users) {
-			if(users[user].uid == userId) {
-				return users[user].userFavorites;
+		for(var user in this.users) {
+			if(this.users[user].uid == userId) {
+				return this.users[user].userFavorites;
 			}	
 		}
+	};
+	
+	this.getAllGroceries = function(){
+		return this.groceries;
+	};
+	
+	this.getAllUsers = function(){
+		return this.users;
 	};
 }
