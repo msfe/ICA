8aa9cefdc4074bd5a92aaa80b1739769c860988f
Mattias <Mattias.Folke@gmail.com>
2014-04-10 14:54:39 +0200

    Headerbar complete

--- apps/ICA/android/native/assets/www/default/worklight/checksum.js
+++ apps/ICA/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":1280432065,"date":1397133739238,"machine":"MattiasDator"};
-/* Date: Thu Apr 10 14:42:19 CEST 2014 */
+var WL_CHECKSUM = {"checksum":1899237796,"date":1397134288960,"machine":"MattiasDator"};
+/* Date: Thu Apr 10 14:51:28 CEST 2014 */
